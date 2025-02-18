7c7da6058ded2e73c01e2e1f4a9f43cdff7eeb47
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-02-17 19:41:25 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import VehiclesScreen from './src/screens/VehiclesScreen';
 
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Vehicles" component={VehiclesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
