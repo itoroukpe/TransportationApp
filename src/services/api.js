4366875b16c4bd9cef2de2889ffa4820ea05d233
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-02-17 21:48:26 -0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api'; // Change to your backend URL
+const API_BASE_URL = 'http://34.209.89.136:8080/api'; // Change to your backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
